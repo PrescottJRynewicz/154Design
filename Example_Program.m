fefa6ab6f921b3bd27177d4e63b2088e52eb1d11
PrescottRynewicz <pjrynewicz@gmail.com>
2017-02-01 18:06:24 -0800

    Added Example codes.

---
+++ Example_Program.m
@@ -0,0 +1,17 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Prescott Rynewicz
+% 504288967
+% Example Function for 154 Design Group
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+% Will use Example Function to calculate and print three numbers. 
+
+
+[x,y,z] = Example_function(1,2,3); 
+disp(x);
+disp(y);
+disp(z);
+
+
+% end of Program
---
+++ Example_function.m
@@ -0,0 +1,8 @@
+function [output1,output2,output3] = Example_function(input1, input2, input3)
+%%This is an example function
+
+output1 = input1*2; 
+output2 = input2*3; 
+output3 = input3*4; 
+
+end
