415c459c8b2e79e5ec69430e164b4623d6d64ef4
PrescottRynewicz <pjrynewicz@gmail.com>
2017-02-07 11:07:18 -0800

    Added CG and Weight_Range Calca from Jordy and Janelle.

---
+++ CG_and_MAC.m
@@ -0,0 +1,156 @@
+
+%%% Overall Aircraft Parameters 
+
+MTOW=93000;                             % Max Take-Off Weight [lb]
+PL=90;                                  % Plane Length [ft]
+WL=90;                                  % Wing Loading [lb/ft^2]
+w_fuel=35214;                           % total fuel weight
+
+% Distances are measured from the nose of the aircraft
+% to the leading edge (LE) of the component
+% units=[ft]
+L_nose=17.93;                           % length of nose
+L_fusel=42.17;                          % length of fuselage
+L_emp=29.9;                             % length of tail empennage
+L_t_vert_root=                          % length of vertical tail surface root chord
+L_t_horiz_root=L_t_vert_root;           % length of horizontal tail surface root chord
+L_eng=13;                               % length of engine
+L_nac=14;                               % length of nacelle
+L_seat1=10;                             % length of seating section 1 (4 people)
+L_seat2=10;                             % length of seating section 2 (4 people)
+L_seat3=10;                             % length of seating section 3 (4 people)
+
+x_wing=                                 % wing LE placement
+x_eng=                                  % engine LE placement
+x_nac=x_eng;                            % nacelle LE placement
+x_lg_front=                             % front landing gear placement
+x_lg_rear=                              % rear landing gear placement
+x_fusel=L_nose;                         % fuselage LE placement                               
+x_emp=L_nose+L_fusel;                   % tail empennage LE placement
+x_t_vert=                               % vertical tail surface LE placement
+x_t_horiz=x_t_vert;                     % horizontal tail surface LE placement
+x_seat_cp=                              % placement of cockpit seating CG (2 pilots)
+x_seat_js=                              % placement of jump seat CG (1 steward(ess))
+                                            % cockpit seating should go around the end of the nose-
+                                            % fuselage just counts cabin in this code so you can put
+                                            % cockpit forward of that; jump seat can go in cabin or cockpit
+x_seat1=                                % placement of seating section 1 LE (4 people)
+x_seat2=                                % placement of seating section 2 LE (4 people)
+x_seat3=                                % placement of seating section 3 LE (4 people)
+                                            % seating sections 1-3 are each approx
+                                            % 10 ft long- section 1 CG must be at least 5ft into the
+                                            % fuselage and section 3 must be at
+                                            % least 5 feet from the end of the fuselage
+                                         
+% Weight Parameters
+% Units are in [lb]
+w_1eng=2445;                            % weight of an indivual engine
+num_eng=2;                              % number of engines
+w_engs=num_eng*w_1eng;                  % total weight of all engines
+
+fuelden=49.943;                         % fuel density of jet fuel [lb/ft^3]
+fuelvol=w_fuel/fuelden;                 % volume of fuel tanks needed
+                                        % See below wing group info for
+                                        % fuel tank weights
+w_wing=.103*MTOW;                       % weight of wing group
+w_tail=.0287*MTOW;                      % weight of tail group
+w_fusel=.122*MTOW;                      % weight of fuselage
+w_sc=.0138*MTOW;                        % weight of surface controls
+w_nac=.0155*MTOW;                       % weight of nacelle
+w_lg=.04*MTOW;                          % weight of landing gear
+
+w_psngrseat=45;                         % weight of luxury passenger seat
+w_js=30;                                % weight of jump seat 
+w_pilotseat=40;                         % weight of pilot seat
+w_psngr=200;                            % weight of passenger and their on-hand items
+
+%%% Wing and Surface Controls Group Parameters
+
+S=MTOW/WL;                              % Wing Area [ft^2]
+AR=7.2015;                              % Aspect Ration
+Cr=sqrt(AR*S)/5;                        % root chord length [ft]
+Ct=0.3886*Cr;                           % tip chord length [ft]
+MAC=.73917*Cr;
+x_MAC=x_wing+((1-.8037)*Cr);            % placement of LE of MAC
+x_wing_cg=x_wing+(.7*Cr);               % placement of wing CG on aircraft
+x_sc=x_MAC+MAC;                         % surface controls CG placement
+
+m_wing=x_wing_cg*w_wing;
+m_sc=x_sc*w_sc;
+
+%%% Fuel Tank Parameters 
+
+w_centfuel=.7*10.18*Cr*fuelden;         % weight of fuel in center fuel tank 
+                                        % 10.18=cross section surface area of cargo hold
+                                        % .7=percentage estimation of available volume
+w_wingfuel=w_fuel-w_centfuel;           % total weight of fuel in both wings
+x_centfuel_cg=x_wing+(Cr/2);            % position of CG for center fuel tank
+x_wingfuel_cg=(x_wing+.5)+(.7*.4*Cr);   % modeled as a mini-wing inside of main wing 
+                                        % offset back by 6in and with length of .4*Cr
+m_centfuel=x_centfuel_cg*w_centfuel;
+m_wingfuel=x_wingfuel_cg*w_wingfuel;
+                                        
+%%% Engine Group Parameters
+
+x_eng_cg=x_eng+(L_eng/2);
+
+m_engs=x_eng_cg*w_engs;
+
+%%% Nacelle Group Parameters
+
+x_nac_cg=x_nac+(.4*L_nac);
+
+m_nac=x_nac_cg*w_nac;
+
+%%% Tail Group Parameters
+
+x_vert_cg=x_t_vert+(.42*L_t_vert_root);
+x_horiz_cg=x_t_horiz+(.42*L_t_horiz_root);
+
+m_tail=x_horiz_cg*w_tail; 
+
+%%% Fuselage Group Parameters
+
+x_fusel_cg=x_fusel+(.47*L_fusel);
+
+m_fusel=w_fusel*x_fusel_cg;
+
+%%% Landing Gear Parameters
+
+x_lg_cg=x_lg_front+(((2/3)*x_lg_rear*w_lg)/w_lg);
+
+m_lg=w_lg*x_lg_cg;
+
+%%% Payload+Pilot Parameters
+
+x_seat1_cg=x_seat1+(L_seat1/2);
+x_seat2_cg=x_seat2+(L_seat2/2);
+x_seat3_cg=x_seat3+(L_seat3/2);
+
+w_seat1=(4*w_psngrseat)+(4*w_psngr);
+w_seat2=(4*w_psngrseat)+(4*w_psngr);
+w_seat3=(4*w_psngrseat)+(4*w_psngr);
+w_pilots=(2*w_pilotseat)+(2*w_psngr);
+w_js_total=w_js+w_psngr;
+
+m_js=w_js_total*x_seat_js;
+m_cp=x_seat_cp*w_pilots;
+m_seat1=x_seat1_cg*w_seat1;
+m_seat2=x_seat2_cg*w_seat2;
+m_seat3=x_seat3_cg*w_seat3;
+
+%%% Sum Moments
+
+sum_m=m_js+m_cp+m_seat1+m_seat2+m_seat3+m_lg+m_fusel+m_tail+m_nac+m_engs+m_wingfuel+m_centfuel+m_wing_m_sc;
+
+%%% Sum Weights
+
+sum_w=w_seat1+w_seat2+w_seat3+w_pilots+w_js_total+w_fuel+w_engs+w_wing+w_tail+w_fusel+w_sc+w_nac+w_lg;
+
+%%% CG Calculation
+
+CG=sum_n/sum_w
+
+%%% Percent MAC Calculation
+
+Percent_MAC=(CG-x_MAC)/MAC
---
+++ Weight_and_Range_New.m
@@ -0,0 +1,99 @@
+clc; clear all; close all;
+
+%% Define Maximum Gross Takeoff Weight
+
+MTOW = 112000;
+
+%% Calculate Group Weights
+
+% group weights were calculated by referencing Torenbeek ch. 8 pg. 280, Aerospace Caravelle VIR
+
+% the Aerospace Caraveller was chosen because its MTOW of 110,230 lbs is closest 
+% to our MTOW of 112,000 lbs
+
+% airframe structure includes: wing group, tail group, fuselage group,
+% landing gear, surface controls group, nacelle group
+
+WingGroup = 0.134*MTOW;
+TailGroup = 0.0177*MTOW;
+FuselageGroup = 0.105*MTOW;
+LandingGear = 0.0463*MTOW;
+SurfaceControlsGroup = 0.0187*MTOW;
+NacelleGroup = 0.0143*MTOW;
+AirframeStructure = WingGroup + TailGroup + FuselageGroup + LandingGear + SurfaceControlsGroup + NacelleGroup;
+
+% propulsion group includes: engines
+
+PropulsionSystem = 2*2445;                                  % [lbs] there are 2 engines, each weighing in at 2445 lbs
+
+%% Define and Calculate Parameters
+
+Wempty = AirframeStructure + PropulsionSystem;
+Empty_2_MTOW_Ratio = Wempty / MTOW;                         % [unitless] this is the ratio of the Empty Weight of the aircraft to the MTOW of the aircraft
+Wleftover = MTOW - Wempty;                                  % [lbs] this is what's available to be split b/w fuel and payload
+Payload = 13*200;                                           % [lbs] 12 passengers + 1 stewardess, each w/ combined body and luggage weight of 200 lbs as suggested by McCormick, ch. 7, pg. 407
+WingLoading = 90;                                           % [lb/ft^2]
+WingArea = (1/WingLoading)*MTOW;                            % [ft^2]
+AspectRatio = 7.2015;                                       % [unitless]
+WingSpan = sqrt(WingArea*AspectRatio);                      % [ft] span of just one wing
+C_r = sqrt(AspectRatio*WingArea)/5;                         % [ft] root chord length
+MAC = 0.73917*C_r;                                          % [ft] mean aerodynamic chord
+CL_CD = 4.289;                                              % [unitless] lift to drag ratio for at M = 1.6, alpha = 2 deg
+a = 573.3502;                                               % [nmi/hr] speed of sound at altitude 45,000 ft
+M = 1.6;                                                    % [unitless] Mach number
+V = M*a;                                                    % [nmi/hr] aircraft cruise speed
+Range = 4500;                                               % [nmi] specified range
+Ctsfc = 0.8766;                                             % [1/hr] thrust specific fuel consumption based off of BJ205 at 45000 ft and Mach 1.6
+
+%% Load Condition 1 - Max Payload & Max Fuel
+
+Waircraft1 = MTOW;                                          % [lbs]
+
+%% Load Condition 2 - Max Payload & Min Fuel
+
+% FAA Regulations state that an aircraft must maintain a minumum 45 minute
+% fuel reserve, so use the endurance equation to calculate how many lbs of
+% fuel that would take
+
+E = 0.75;                                                   % [hr]
+Wfuel45min = MTOW - (MTOW/(exp(E/((1/Ctsfc)*(CL_CD)))));    % [lbs]
+Waircraft2 = Wempty + Wfuel45min + Payload;                 % [lbs]
+
+%% Load Condition 3 - Min Payload & Max Fuel
+
+% Min payload can be assumed to be 0 since the 2 pilots are included in the
+% dry weight of the aircraft
+
+Waircraft3 = MTOW - Payload;
+
+%% Load Condition 4 - Min Payload & Min Fuel
+
+Waircraft4 = Wempty + Wfuel45min;                           % [lbs]
+
+%% Calculate weight of fuel needed to satisfy range
+
+W0 = 112000;                                                % [lbs] MTOW
+R = 4500;                                                   % [nmi] range
+a0 = 661.47;                                                % [nmi/hr] speed of sound @ sea level
+M = 1.6;                                                    % [unitless] mach number
+L_D = 4.289;                                                % [unitless] lift to drag ratio
+Ctsfc = 0.8766;                                             % [1/hr] thrust specific fuel consumption
+Ta = 216.65;                                                % [K] ambient temp @ cruising altitude
+T0 = 288.15;                                                % [K] std atmospheric temp
+
+% using eq 2.130 from O. O. Bendiksen lecture 2 slides
+
+W1 = W0/exp((R/a0)/((M*(L_D))/(Ctsfc / sqrt(Ta/T0))));
+Wfuelneeded = W0 - W1;
+
+%% Error in fuel prediction
+
+Wfuelmax = MTOW - (Wempty + Payload);                       % [lbs] this should be all that is theoretically available
+                                                            % based on volume constraints
+x = Wfuelmax - Wfuelneeded;
+if x < 0
+    x = abs(x);
+    disp(['We are over weight limits for fuel by ' num2str(x)])
+else
+    disp(['We are under weight limits for fuel by ' num2str(x)])
+end
