effc3d5b9c8777ac207a0d7a60b0adcb6672e954
PrescottRynewicz <pjrynewicz@gmail.com>
2017-03-13 12:12:39 -0700

    Added different revision of Mother Bird.

--- Take_Off_and_Landing.m
+++ Take_Off_and_Landing.m
@@ -6,10 +6,7 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 close all; clear all; clc;
 
-
-%% Variable Definition
-
-
+%% Take Off Calcs
 MTOW        = 70000;                %[lb]  : Max Take Off
 CL_Max      = 1.8;                  %[  ]  : Max CL, based on approximations from other aircraft
 S           = 636.364;              %[ft^2]: Wing Area
@@ -17,7 +14,7 @@ rho         = .00238;               %[slug/ft^3]: Density at sea level
 V_T0        = sqrt(2*MTOW/S...
     /CL_Max/rho);                   %[ft/s]: Velocity to be able to take off
 a           = 1190.646;             %[ft/s]: speed of sound at sea level
-q           = 0.5*rho*(0.7*V_T0)^2; %[
+q           = 0.5*rho*(0.7*V_T0)^2; %[    ]: Dynamic Pressure 
 M           = 0.7*V_T0/a;           %[    ]: Mach at take off speed
 t_r         = 3;                    %[s]   : roll time 
 g           = 32.2;                 %[ft/s^2]: 
@@ -26,12 +23,18 @@ h           = 35;                   %[ft]: Clearing Height
 C_D         = .2236;                %[  ]: Drag Coefficient
 D           = q*S*C_D;              %[lb]: Drag at Takeoff
 T           = 21900*2;              %[lb]: Thrust at Takeoff
-gamma_cl    = asin((T-D)/MTOW);     %[ ] : Something
-mu          = 0.03;                 %[ ] : Coefficient of Ground Friction
+gamma_cl    = asin((T-D)/MTOW);     %[  ]: Something
+mu          = 0.03;                 %[  ]: Coefficient of Ground Friction
 L           = q*S*CL_Max;           %[lb]: Average lift
 T_e         = T-D-mu*(MTOW-L);      %[lb]: Excess thrust during takeoff
 U           = 0.7*V_T0;             %[ft/s]: Average Speed
 S           = 0.5*MTOW*V_T0^2/T_e/32.3 + V_T0*t_r + ...
     R*sin(gamma_cl) + h - R*(1 - cos(gamma_cl))/tan(gamma_cl);
+
+
+% S is the total runway length required. All of this is based off of
+% equations from Lukas and the initial H-something approximations. 
+% Need to find updated values from slides for how to do new integral take
+% off stuff. 
 
 
